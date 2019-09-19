746ca4d15b42377b761e9b7e6abfc7a034dbfbc2
JonAnder-Pc <JonAnder-Pc@DESKTOP-EQF43GB>
2019-09-19 11:29:44 +0200

    Singletons de lista de actores y peliculas

--- src/GestionWeb/ListaActores.java
+++ src/GestionWeb/ListaActores.java
@@ -1,5 +1,18 @@
 package GestionWeb;
 
+import java.util.*;
+
 public class ListaActores {
 
+	private static ListaActores mListaActores = null;
+	private ArrayList<Actor> lista;
+	
+	private ListaActores() {
+		lista = new ArrayList<Actor>();
+	}
+	
+	public static ListaActores getListaActores() {
+		if(mListaActores == null) mListaActores = new ListaActores();
+		return mListaActores;
+	}
 }
--- src/GestionWeb/ListaPeliculas.java
+++ src/GestionWeb/ListaPeliculas.java
@@ -1,5 +1,19 @@
 package GestionWeb;
 
+import java.util.*;
+
 public class ListaPeliculas {
 
+	private static ListaPeliculas mListaPeliculas = null;
+	private ArrayList<Pelicula> lista;
+	
+	private ListaPeliculas() {
+		lista = new ArrayList<Pelicula>();
+	}
+	
+	public static ListaPeliculas getListaPeliculas() {
+		if(mListaPeliculas == null) mListaPeliculas = new ListaPeliculas();
+		return mListaPeliculas;
+	}
+	
 }
