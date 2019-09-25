571f4ce34d55f97ff694d89c827c3dbd7cf9a77c
JonAnder-Pc <JonAnder-Pc@DESKTOP-EQF43GB>
2019-09-25 15:21:20 +0200

    ListaPeliculas bien espaciado

--- src/GestionWeb/ListaPeliculas.java
+++ src/GestionWeb/ListaPeliculas.java
@@ -1,19 +1,27 @@
 package GestionWeb;
 
 import java.util.*;
+import java.io.*;
 
 public class ListaPeliculas {
 
 	private static ListaPeliculas mListaPeliculas = null;
 	private HashMap<String, Pelicula> lista;
-	
+
 	private ListaPeliculas() {
 		lista = new HashMap<String, Pelicula>();
 	}
-	
+
 	public static ListaPeliculas getListaPeliculas() {
-		if(mListaPeliculas == null) mListaPeliculas = new ListaPeliculas();
+		if (mListaPeliculas == null)
+			mListaPeliculas = new ListaPeliculas();
 		return mListaPeliculas;
 	}
-	
+
+	public HashMap<String, Pelicula> getHash() {
+
+		return lista;
+
+	}
+
 }
