45dfb26ed5693b60b91d36f712f9949f107b46a2
Jon Ander <jmiguel011@ikasle.ehu.eus>
2019-10-23 17:28:25 +0200

    Puesto los datos pasados

--- src/Practica2/OrderedListADT.java
+++
@@ -1,7 +0,0 @@
-package Practica2;
-
-public interface OrderedListADT<T> extends ListADT<T>{
-	
-	public void add(T elem);
-	public void merge(OrderedLinkedList<T> lista);
-}
--- src/Practica2/UnorderedLinkedList.java
+++
@@ -1,8 +0,0 @@
-package Practica2;
-
-public interface UnorderedLinkedList<T> extends ListADT<T>{
-
-	public void addToFront(T elem);
-	public void addToRear(T elem);
-	public void addAfter(T elem, T target);
-}
