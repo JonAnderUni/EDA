d1f1c42edccb559aa7d7cb1a37975eddd0467448
JonSenapi <JonSenapi@DESKTOP-QR8425D>
2019-10-31 18:59:18 +0100

    Metodo add lista ordenada

--- src/segundaFase/OrderedDoubleLinkedList.java
+++ src/segundaFase/OrderedDoubleLinkedList.java
@@ -1,16 +1,58 @@
 package segundaFase;
 
+
 public class OrderedDoubleLinkedList<T> extends DoubleLinkedList<T> implements OrderedListADT<T> {
 	
-	public void add(T elem){}
+	public OrderedDoubleLinkedList(){
+		first = null;
+		last = null;
+		descr= "";
+		count = 0;
+	}
+	public void add(T elem){
 		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
-
+		if(first == null) {
+			first = new Node<T>(elem);
+			last = first;
+		}else{
+			Node<T> nuevo = new Node<T>(elem);
+			Node<T> act = first;
+			if(nuevo.data.toString().compareTo(act.toString()) < 0) {
+				nuevo.next = act;
+				nuevo.prev = act.prev;
+				act.prev.next = nuevo;
+				act.prev = nuevo;
+				first = nuevo;
+			}else{
+				act = act.next;
+				while(act != first) {
+					if(nuevo.data.toString().compareTo(act.toString()) < 0) {
+						nuevo.next = act;
+						nuevo.prev = act.prev;
+						act.prev.next = nuevo;
+						act.prev = nuevo;
+					}
+				}
+			}
+			if(contains(elem) == false) {
+				nuevo.next = first;
+				nuevo.prev = last;
+				last.next = nuevo;
+				first.prev = nuevo;
+				last = nuevo;
+			}
+			last = first.prev;
+		}
+	}
 	public void merge(DoubleLinkedList<T> zerrenda){
 
 
 
 	}
+	
+	
+	
+	
 
 
 }
